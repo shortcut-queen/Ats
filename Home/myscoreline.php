a94aa8406c1ccdb6fe6f3c638a9c3db4e3d67a58
sevenHsu <jetxu24@icloud.com>
2018-05-04 16:45:36 +0800

    xushen:4-29,16:04

---
+++ Home/myscoreline.php
@@ -0,0 +1,84 @@
+<?php
+//未登录返回登陆页面
+session_start();
+if(!isset($_SESSION['user_id']))
+    header('location:index.php');
+use Ats\Service\ProjectService;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>用户:<?php echo $_SESSION['user_name']; ?></title>
+    <link rel="stylesheet" href="../Static/css/bootstrap.min.css">
+    <script src="../Static/js/bootstrap.min.js"></script>
+    <script src="../Static/js/jquery-3.2.1.js"></script>
+    <script src="../Static/js/echarts.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#buttonSearch").click(function(){
+                $.post(document.myScoreLine.action,
+                    {
+                        form_name:document.myScoreLine.form_name.value,
+                        startDate:document.myScoreLine.startDate.value,
+                        endDate:document.myScoreLine.endDate.value,
+                        project:document.scoreTermSearch.project.value
+                    },
+                    function(data){
+                        document.getElementById('myScoreDiv').innerHTML=data;
+                        var dataList={date:[],score:[]};
+                        for(var i=0;;i++){
+                            var date_input=document.getElementById('date'+i);
+                            if(date_input==null)
+                                break;
+                            else {
+                                dataList['date'][i]=date_input.name;
+                                dataList['score'][i]=parseInt(date_input.value);
+                            }
+                        }
+                        var myChart = echarts.init(document.getElementById('showLine'));
+                        option = {
+                            xAxis: {
+                                type: 'category',
+                                boundaryGap: false,
+                                data: dataList['date']
+                            },
+                            yAxis: {
+                                type: 'value'
+                            },
+                            series: [{
+                                data: dataList['score'],
+                                type: 'line',
+                                areaStyle: {}
+                            }]
+                        };
+                        myChart.setOption(option);
+                    });
+            });
+        });
+    </script>
+</head>
+<body>
+<?php include("usernav.php") ?>
+<div style="position: relative;width:100%;text-align:center;font-size:large;margin-top: 4%">折线分析</div>
+<div style="position: relative;margin-top: 4%;text-align: center">
+    <?php
+    include("../Service/ProjectService.php");
+    //查询所有项目名称
+    $result=ProjectService::selectAllProject();
+    echo"<form class='form-inline' name='myScoreLine' action='../Web/UserController.php' method='post'>";
+    echo "<input type='hidden' name='form_name' value='myScoreLine'/>";
+    echo "<input class='form-control' type='date' name='startDate'/>";
+    echo "<input class='form-control' type='date' name='endDate'/>";
+    echo "<select class='form-control' name='project'><option value='all_project'>--项目--</option>";
+    while ($row=mysql_fetch_array($result))
+        echo "<option value='".$row['Project_Id']."'>".$row['Project_Name']."</option>";
+    echo "</select>";
+    echo "<button id='buttonSearch' class='btn btn-primary' type='button'>查询</button>";
+    echo "</form>";
+    ?>
+</div>
+<div id="myScoreDiv"></div>
+<div id="showLine" style="width: 100%;height: 400px"></div>
+</body>
+</html>
--- Home/usernav.php
+++ Home/usernav.php
@@ -1,6 +1,49 @@
 <link rel="stylesheet" href="../Static/css/bootstrap.min.css">
 <script src="../Static/js/bootstrap.min.js"></script>
 <script src="../Static/js/jquery-3.2.1.js"></script>
+<style type="text/css">
+    .dropdown-submenu {
+        position: relative;
+    }
+    .dropdown-submenu > .dropdown-menu {
+        top: 0;
+        left: 100%;
+        margin-top: -6px;
+        margin-left: -1px;
+        -webkit-border-radius: 0 6px 6px 6px;
+        -moz-border-radius: 0 6px 6px;
+        border-radius: 0 6px 6px 6px;
+    }
+    .dropdown-submenu:hover > .dropdown-menu {
+        display: block;
+    }
+    .dropdown-submenu > a:after {
+        display: block;
+        content: " ";
+        float: right;
+        width: 0;
+        height: 0;
+        border-color: transparent;
+        border-style: solid;
+        border-width: 5px 0 5px 5px;
+        border-left-color: #ccc;
+        margin-top: 5px;
+        margin-right: -10px;
+    }
+    .dropdown-submenu:hover > a:after {
+        border-left-color: #fff;
+    }
+    .dropdown-submenu.pull-left {
+        float: none;
+    }
+    .dropdown-submenu.pull-left > .dropdown-menu {
+        left: -100%;
+        margin-left: 10px;
+        -webkit-border-radius: 6px 0 6px 6px;
+        -moz-border-radius: 6px 0 6px 6px;
+        border-radius: 6px 0 6px 6px;
+    }
+</style>
 <nav class="navbar navbar-default navbar-fixed-top " role="navigation" style=" font-size:18px;background-color: #46b8da;">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="bs-example-navbar-collape-1">
@@ -10,9 +53,9 @@
                 </li>
                 <?php
                 if(intval($_SESSION['officer'])!=0)
-                    echo "<li class='dropdown' role='presentation' tyle='width: 200px'><a href='' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>&emsp;成绩分析&emsp;<span class='caret'></a><ul class='dropdown-menu'><li style='background-color: #46b8da; font-size: 18px;'><a href='myscore.php'>个人成绩</a></li><li style='background-color: #46b8da; font-size: 18px;'><a href='score.php'>成绩查询</a></li><li style='background-color: #46b8da;font-size: 18px;'><a href='scorecompare.php'>图表对比</a></li><li style='background-color: #46b8da;font-size: 18px;'><a href='scoreterm.php'>折线分析</a></li></ul></li>";
+                    echo "<li class='dropdown' role='presentation' tyle='width: 200px'><a href='' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>&emsp;成绩分析&emsp;<span class='caret'></a><ul class='dropdown-menu'><li class='dropdown-submenu' style='background-color: #46b8da; font-size: 18px;'><a tabindex='-1' href='javascript:;'>个人成绩</a><ul class='dropdown-menu'><li style='background-color: #46b8da;font-size: 18px;'><a href='myscore.php'>成绩查询</a></li><li style='background-color: #46b8da;font-size: 18px;'><a href='myscoreline.php'>成长分析</a></li></ul></li><li style='background-color: #46b8da; font-size: 18px;'><a href='score.php'>成绩查询</a></li><li style='background-color: #46b8da;font-size: 18px;'><a href='scorecompare.php'>图表对比</a></li><li style='background-color: #46b8da;font-size: 18px;'><a href='scoreterm.php'>折线分析</a></li></ul></li>";
                 else
-                    echo "<li><a href='myscore.php'>&emsp;个人成绩&emsp;</a></li>";
+                    echo "<li class='dropdown' role='presentation' tyle='width: 200px'><a href='' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>&emsp;个人成绩&emsp;<span class='caret'></a><ul class='dropdown-menu' style='width:inherit;'><li style='background-color: #46b8da;font-size: 18px;'><a href='myscore.php'>成绩查询</a></li><li style='background-color: #46b8da;font-size: 18px;'><a href='myscoreline.php'>成长分析</a></li></ul></li>";
                 ?>
                 <li>
                     <a href="resource.php" >&emsp;资源库&emsp;</a>
